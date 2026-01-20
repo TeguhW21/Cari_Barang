f4775cce06cf92de377d936997cf859bd3178308
Teguh Wahidin <teguhwahidin098@gmail.com>
2026-01-20 15:21:25 +0700

    Push index

---
+++ index.php
@@ -0,0 +1,367 @@
+<?php
+$WA_NUMBER = "6285974647282";
+
+// DATA LENGKAP SESUAI TEKS ANDA
+$barang = [
+    ["nama" => "Polycup", "harga" => 4000, "stok" => "46 Pak"],
+    ["nama" => "Freshware 1000 ml", "harga" => 24000, "stok" => "8 pak"],
+    ["nama" => "Nampan Mini", "harga" => 21000, "stok" => "22 Pak"],
+    ["nama" => "Mika GX-5", "harga" => 10000, "stok" => "20 Pak"],
+    ["nama" => "Cup Ajaib", "harga" => 0, "stok" => "21 pak"],
+    ["nama" => "Tray Nasi Mitra 16x22", "harga" => 8500, "stok" => "20 Pak"],
+    ["nama" => "Mika Tabung 10 cm", "harga" => 16500, "stok" => "17 pak"],
+    ["nama" => "Sifon Mika 18", "harga" => 65000, "stok" => "7 pak"],
+    ["nama" => "Tray 18", "harga" => 7000, "stok" => "15 pak"],
+    ["nama" => "Tray 20", "harga" => 8000, "stok" => "30 Pak"],
+    ["nama" => "Freshware 2000 ml", "harga" => 55000, "stok" => "5 pak"],
+    ["nama" => "CT 10", "harga" => 13000, "stok" => "27 pak"],
+    ["nama" => "Sendok Puding", "harga" => 3500, "stok" => "12 Pak"],
+    ["nama" => "Karet merah Gorin", "harga" => 6000, "stok" => "25 Pak"],
+    ["nama" => "Cake case 7 corak", "harga" => 11000, "stok" => "20 Pak"],
+    ["nama" => "Tisu Gaby", "harga" => 8000, "stok" => "10 Pak"],
+    ["nama" => "Standing Poch 9x15", "harga" => 7500, "stok" => "52 Pak"],
+    ["nama" => "Sarung tangan plastik", "harga" => 7500, "stok" => "42 Pak"],
+    ["nama" => "Tray Sambal", "harga" => 6500, "stok" => "28 Pak"],
+    ["nama" => "DM 120 ml SQ", "harga" => 13000, "stok" => "11 Pak"],
+    ["nama" => "Sendok Makan", "harga" => 4000, "stok" => "64 Pak"],
+    ["nama" => "Sendok The Fim", "harga" => 4500, "stok" => "80 Pak"],
+    ["nama" => "CT 15 O", "harga" => 3000, "stok" => "150 Pcs"],
+    ["nama" => "CT 20 Kotak", "harga" => 4300, "stok" => "50 Pcs"],
+    ["nama" => "Bowl Ajaib 800 ml", "harga" => 21650, "stok" => "11 Pak"],
+    ["nama" => "Lid Bowl Ajaib", "harga" => 0, "stok" => "-"],
+    ["nama" => "Bowl 650 Daun", "harga" => 25000, "stok" => "3 Pak"],
+    ["nama" => "Foodpack 600 ml", "harga" => 30000, "stok" => "10 Pak"],
+    ["nama" => "Starglas 22 oz", "harga" => 14000, "stok" => "20 Pak"],
+    ["nama" => "CT 28 Kotak", "harga" => 71600, "stok" => "2 Pak"],
+    ["nama" => "Cup Merak 22 oz", "harga" => 20750, "stok" => "14 pak"],
+    ["nama" => "BSM 16 Oz", "harga" => 11000, "stok" => "14 pak"],
+    ["nama" => "Mika Tabung 7 cm", "harga" => 16000, "stok" => "5 Pak"],
+    ["nama" => "Mika Tabung 12 cm", "harga" => 17500, "stok" => "5 Pak"],
+    ["nama" => "Stik Es", "harga" => 14000, "stok" => "26 Pak"],
+    ["nama" => "Mika Segitiga", "harga" => 28000, "stok" => "10 Pak"],
+    ["nama" => "Sedotan Tekuk", "harga" => 1000, "stok" => "23 Pak"],
+    ["nama" => "DM 1.010 SQ", "harga" => 70000, "stok" => "16 Pak"],
+    ["nama" => "CT 28 O", "harga" => 73000, "stok" => "4 Pak"],
+    ["nama" => "CT 25 O", "harga" => 64000, "stok" => "4 Pak"],
+    ["nama" => "Cup Puding 65 ml", "harga" => 5000, "stok" => "39 Pak"],
+    ["nama" => "DM 300 R/b", "harga" => 14000, "stok" => "7 Pak"],
+    ["nama" => "Jar 800 ml", "harga" => 3000, "stok" => "33 Pcs"],
+    ["nama" => "Jar 1000 ml", "harga" => 3500, "stok" => "12 Pcs"],
+    ["nama" => "Nuai Ratu", "harga" => 15000, "stok" => "4 Dus"],
+    ["nama" => "Nuai Oval", "harga" => 0, "stok" => "7 Dus"],
+    ["nama" => "Delice Apple", "harga" => 0, "stok" => "6 Dus / 8 Pcs"],
+    ["nama" => "Toples Logo Tabung 317", "harga" => 0, "stok" => "3 Dus / 7 Pcs"],
+    ["nama" => "Easytop Tabung 815", "harga" => 0, "stok" => "1 Dus / 5 Pcs"],
+    ["nama" => "Nuai Gemini sekat 2", "harga" => 0, "stok" => "4 Dus"],
+    ["nama" => "Golden Square pendek", "harga" => 0, "stok" => "1 Dus / 3 Pcs"],
+    ["nama" => "Golden Bulat sekat 3", "harga" => 0, "stok" => "18 Pcs"],
+    ["nama" => "Golden Tabung 112", "harga" => 0, "stok" => "11 Pcs"],
+    ["nama" => "Logo 312", "harga" => 0, "stok" => "9 Pcs"],
+    ["nama" => "Logo 319", "harga" => 0, "stok" => "1 Dus"],
+    ["nama" => "Golden ½ kg", "harga" => 0, "stok" => "1 Dus"],
+    ["nama" => "Golden Bulat Gepeng", "harga" => 0, "stok" => "1 Dus"],
+    ["nama" => "Rajawali", "harga" => 0, "stok" => "9 Pcs"],
+    ["nama" => "Golden Wajik", "harga" => 0, "stok" => "10 Pcs"],
+    ["nama" => "Sekat Berkah", "harga" => 0, "stok" => "4 Pcs"],
+    ["nama" => "Nuai SN 299", "harga" => 0, "stok" => "4 Pcs"],
+    ["nama" => "TPS Draco 18x18", "harga" => 0, "stok" => "2 Pak"],
+    ["nama" => "R3 Putih", "harga" => 0, "stok" => "2 Pak"],
+    ["nama" => "Duplek Pizza", "harga" => 0, "stok" => "35 Pcs"],
+    ["nama" => "Hansel Pizza 20x20", "harga" => 1500, "stok" => "48 Pcs"],
+    ["nama" => "Hansel Pizza 22x22", "harga" => 2000, "stok" => "329 Pcs"],
+    ["nama" => "Hansel Pizza 25x25", "harga" => 2500, "stok" => "56 Pcs"],
+    ["nama" => "Hansel Donat 27x18.5", "harga" => 2000, "stok" => "260 Pcs"],
+    ["nama" => "Hansel Donat 27.5x9.5", "harga" => 1500, "stok" => "233 Pcs"],
+    ["nama" => "Lux Brown Mika 18x18", "harga" => 2000, "stok" => "0"],
+    ["nama" => "Lux Brown Mika 20x20", "harga" => 2500, "stok" => "176 Pcs"],
+    ["nama" => "Lux Brown Mika 22x22", "harga" => 2700, "stok" => "71 Pcs"],
+    ["nama" => "Hansel Roti Bakar", "harga" => 850, "stok" => "332 Pcs"],
+    ["nama" => "Donat UP", "harga" => 0, "stok" => "100 Pcs"],
+    ["nama" => "Berkah Kraft Brownis M", "harga" => 1500, "stok" => "66 Pcs"],
+    ["nama" => "Gretel Lux Brownies 22x10x5", "harga" => 2000, "stok" => "19 Pcs"],
+    ["nama" => "Gretel Lux Brownies 30x10x5", "harga" => 2500, "stok" => "28 Pcs"],
+    ["nama" => "Dus Chicken", "harga" => 0, "stok" => "20 Pcs"],
+    ["nama" => "Eklusive Hijau 12x16", "harga" => 800, "stok" => "80 Pcs"],
+    ["nama" => "Eklusive corak lama 20x20", "harga" => 1000, "stok" => "120 Pcs"],
+    ["nama" => "Caecar R donat", "harga" => 0, "stok" => "70 Pcs"],
+    ["nama" => "HD Roll Buah 25x38", "harga" => 52000, "stok" => "6 Roll"],
+    ["nama" => "Eklusive 12x14", "harga" => 800, "stok" => "145 Pcs"],
+    ["nama" => "Eklusive 12x16", "harga" => 900, "stok" => "100 Pcs"],
+    ["nama" => "Eklusive 20x20", "harga" => 1400, "stok" => "210 Pcs"],
+    ["nama" => "Eklusive 18x18", "harga" => 1300, "stok" => "470 Pcs"],
+    ["nama" => "Lux Gretel 18x18", "harga" => 4000, "stok" => "14 Pak"],
+    ["nama" => "Lux Gretel 22x22", "harga" => 5000, "stok" => "6 Pak"],
+    ["nama" => "Mika 22 Merah", "harga" => 5000, "stok" => "10 Pcs"],
+    ["nama" => "Mika 30 Merah", "harga" => 7000, "stok" => "25 Pcs"],
+    ["nama" => "Gretel Brown 12x12", "harga" => 550, "stok" => "15 Pcs"],
+    ["nama" => "Gretel Brown 12x14", "harga" => 600, "stok" => "159 Pcs"],
+    ["nama" => "Gretel Brown 12x16", "harga" => 650, "stok" => "216 Pcs"],
+    ["nama" => "Gretel Prime R3ba", "harga" => 650, "stok" => "194 Pcs"],
+    ["nama" => "Gretel Prime R5", "harga" => 700, "stok" => "218 Pcs"],
+    ["nama" => "Cemara 18x18", "harga" => 43000, "stok" => "3 Pak"],
+    ["nama" => "Hansel 18x18", "harga" => 62000, "stok" => "3 Pak + 30 Pcs"],
+    ["nama" => "Hansel 20x20", "harga" => 71750, "stok" => "5 Pak"],
+    ["nama" => "Hansel 22x22", "harga" => 73000, "stok" => "3 Pak"],
+    ["nama" => "TPS 18x18", "harga" => 55000, "stok" => "9 Pak"],
+    ["nama" => "TPS 20x20", "harga" => 0, "stok" => "1 Pak"],
+    ["nama" => "TPS 22x22", "harga" => 0, "stok" => "1 Pak"],
+    ["nama" => "TPS 25x25", "harga" => 0, "stok" => "5 Pak"],
+    ["nama" => "Gretel Brown 18x18", "harga" => 1000, "stok" => "390 Pcs"],
+    ["nama" => "Gretel Brown 20x20", "harga" => 1300, "stok" => "580 Pcs"],
+    ["nama" => "Draco 12x14", "harga" => 410, "stok" => "114 Pcs"],
+    ["nama" => "Draco 12x16", "harga" => 530, "stok" => "836 Pcs"],
+    ["nama" => "Dus Nasi Padang 18x18", "harga" => 0, "stok" => "437 Pcs"],
+    ["nama" => "Caecar R3 pink hijau", "harga" => 0, "stok" => "348 Pcs"],
+    ["nama" => "Caecar R3ba pink hijau", "harga" => 0, "stok" => "147 Pcs"],
+    ["nama" => "Caecar R5 pink hijau", "harga" => 0, "stok" => "286 Pcs"],
+    ["nama" => "Pink totol 11x15", "harga" => 500, "stok" => "230 Pcs"],
+    ["nama" => "Caecar Batik 15x10", "harga" => 550, "stok" => "10 Pcs"],
+    ["nama" => "Caecar batik 14x12", "harga" => 600, "stok" => "98 Pcs"],
+    ["nama" => "Mitra Kraft Bali 18x18", "harga" => 950, "stok" => "197 pcs"],
+    ["nama" => "Mitra Kute 16x22", "harga" => 1200, "stok" => "110 pcs"],
+    ["nama" => "Mitra Bali 16x22", "harga" => 1200, "stok" => "29 pcs"],
+    ["nama" => "Mitra 16x22", "harga" => 1200, "stok" => "77 pcs"],
+    ["nama" => "Kraft Bali 20x20", "harga" => 1100, "stok" => "27 pcs"],
+    ["nama" => "Yummy Plaid 12x14", "harga" => 0, "stok" => "50 pcs"],
+    ["nama" => "14x12 Hijau", "harga" => 0, "stok" => "17 pcs"],
+    ["nama" => "Merah", "harga" => 0, "stok" => "14 pcs"],
+    ["nama" => "Caecar 14x12", "harga" => 0, "stok" => "109 pcs"],
+    ["nama" => "Caecar batik R3ba", "harga" => 630, "stok" => "42 pcs"],
+    ["nama" => "Caecar batik R5", "harga" => 650, "stok" => "100 pcs"],
+    ["nama" => "Gretel merah putih R3", "harga" => 550, "stok" => "130 pcs"],
+    ["nama" => "Gretel merah putih R3ba", "harga" => 630, "stok" => "47 pcs"],
+    ["nama" => "Gretel merah putih R5", "harga" => 650, "stok" => "14 pcs"],
+    ["nama" => "Caecar Batik GS 14x12", "harga" => 650, "stok" => "50 pcs"],
+    ["nama" => "Gretel gs merah putih 12x12", "harga" => 500, "stok" => "120 pcs"],
+    ["nama" => "Gretel gs merah putih 12x14", "harga" => 650, "stok" => "187 pcs"],
+    ["nama" => "Caecar Batik R8", "harga" => 1300, "stok" => "65 pcs"],
+    ["nama" => "Gretel merah putih R8", "harga" => 1300, "stok" => "550 pcs"],
+    ["nama" => "Gretel merah putih R10", "harga" => 1400, "stok" => "312 pcs"],
+    ["nama" => "Caecar Batik R10", "harga" => 1400, "stok" => "40 pcs"],
+    ["nama" => "Dus Putih 16x16", "harga" => 0, "stok" => "4 Pak 23 pcs"],
+    ["nama" => "Dus Putih R3ba 12x14", "harga" => 0, "stok" => "15 Pak"],
+    ["nama" => "Dus Putih R3 10x15", "harga" => 0, "stok" => "10 Pak"],
+    ["nama" => "Dus Putih 18x18", "harga" => 0, "stok" => "9 Pak"],
+    ["nama" => "Dus Putih 20x20", "harga" => 0, "stok" => "1 Pak"],
+    ["nama" => "Dus Putih 24x24", "harga" => 0, "stok" => "3 Pak"],
+    ["nama" => "Dus Putih 25x25", "harga" => 0, "stok" => "3 Pak"],
+    ["nama" => "Dus Putih 26x26", "harga" => 0, "stok" => "1 Pak"],
+    ["nama" => "Dus Putih 27 Tinggi", "harga" => 0, "stok" => "3 Pak"],
+    ["nama" => "Dus Putih 30x30", "harga" => 0, "stok" => "6 Pak"],
+    ["nama" => "Dus Putih 35x35", "harga" => 0, "stok" => "4 Pak"],
+    ["nama" => "DM 400 R/B", "harga" => 15000, "stok" => "2 pak"],
+    ["nama" => "DM 450 R/B", "harga" => 16000, "stok" => "5 pak"],
+    ["nama" => "DM 200 Rec", "harga" => 15000, "stok" => "2 pak"],
+    ["nama" => "Trash Bag 80x120", "harga" => 20000, "stok" => "7 pak"],
+    ["nama" => "Trash Bag 60x100", "harga" => 20000, "stok" => "18 pak"],
+    ["nama" => "Million Buram", "harga" => 13000, "stok" => "27 pak"],
+    ["nama" => "Rapia Ajaib", "harga" => 17000, "stok" => "2 pak"],
+    ["nama" => "Paper Rice L", "harga" => 26000, "stok" => "1 pak"],
+    ["nama" => "Lunch box daun L", "harga" => 98000, "stok" => "1 pak"],
+    ["nama" => "Lunch box daun M", "harga" => 92000, "stok" => "4 pak"],
+    ["nama" => "DM 500 S/Q", "harga" => 26000, "stok" => "1 pak"],
+    ["nama" => "Food Pack 750 (isi 50)", "harga" => 0, "stok" => "3 pak"],
+    ["nama" => "DM 200 R/B", "harga" => 11125, "stok" => "5 pak"],
+    ["nama" => "DM 1010 S/Q", "harga" => 68750, "stok" => "2 pak"],
+    ["nama" => "DM 650 ml", "harga" => 26000, "stok" => "1 pak"],
+    ["nama" => "Food Pack 1055 ml", "harga" => 48000, "stok" => "5 pak"],
+    ["nama" => "Tulip 50 Putih", "harga" => 31000, "stok" => "19 pak"],
+    ["nama" => "Tulip 50 Hitam", "harga" => 31000, "stok" => "5 pak"],
+    ["nama" => "Tulip 40 Hitam", "harga" => 18000, "stok" => "23 pak"],
+    ["nama" => "Tulip 35 Hitam", "harga" => 18000, "stok" => "30 pak"],
+    ["nama" => "CT 20 bulat", "harga" => 4000, "stok" => "48 pcs"],
+    ["nama" => "CT 20 kotak", "harga" => 5000, "stok" => "86 pcs"],
+    ["nama" => "CT 15 bulat", "harga" => 4000, "stok" => "6 pcs"],
+    ["nama" => "Mika bgp 923", "harga" => 3000, "stok" => "140 pcs"],
+    ["nama" => "Nampan Kecil", "harga" => 5000, "stok" => "36 pcs"],
+    ["nama" => "Mika Burger Lux", "harga" => 9000, "stok" => "2 pak"],
+    ["nama" => "Nampan Sedang", "harga" => 6000, "stok" => "13 pcs"],
+    ["nama" => "CT 28 bulat", "harga" => 8500, "stok" => "2 pcs"],
+    ["nama" => "CT 25 bulat", "harga" => 7000, "stok" => "-"],
+    ["nama" => "CT 22 bulat", "harga" => 6000, "stok" => "-"],
+    ["nama" => "CT 30 kotak", "harga" => 9000, "stok" => "17 pcs"],
+    ["nama" => "CT 15 bulat", "harga" => 4000, "stok" => "23 pcs"],
+    ["nama" => "Brownis S", "harga" => 1000, "stok" => "15 pcs"],
+    ["nama" => "Brownis M", "harga" => 1200, "stok" => "5 pcs"],
+    ["nama" => "Brownis L", "harga" => 1500, "stok" => "15 pcs"],
+    ["nama" => "Mika 4x", "harga" => 13000, "stok" => "4 pak"],
+    ["nama" => "Bento", "harga" => 1000, "stok" => "17 pck / 20 pcs"],
+    ["nama" => "BGT 25", "harga" => 22000, "stok" => "-"],
+    ["nama" => "CT Oval", "harga" => 2000, "stok" => "10 pcs"],
+    ["nama" => "BG 225", "harga" => 1500, "stok" => "10 pcs"],
+    ["nama" => "Mika 2A", "harga" => 800, "stok" => "35 pcs"],
+    ["nama" => "Mika 1A", "harga" => 1500, "stok" => "41 pcs"],
+    ["nama" => "Sifon Mika 18", "harga" => 1500, "stok" => "45 pcs"],
+    ["nama" => "Sifon Mika 22", "harga" => 75000, "stok" => "1 pak / 35 pcs"],
+    ["nama" => "Spunbon 25x35", "harga" => 0, "stok" => "3 pak"],
+    ["nama" => "REA 20", "harga" => 16000, "stok" => "26 pak"],
+    ["nama" => "Paper Grande", "harga" => 27000, "stok" => "9 pak"],
+    ["nama" => "Kertas nasi singa 22x26", "harga" => 13000, "stok" => "17 pak"],
+    ["nama" => "Kertas nasi singa 25x27", "harga" => 15000, "stok" => "8 pak"]
+];
+?>
+<!doctype html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <title>Katalog Produk Plastik</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <style>
+        :root {
+            --primary-color: #0d6efd;
+            --wa-color: #25D366;
+            --ask-color: #ff9f43;
+        }
+
+        body {
+            background-color: #f4f7f6;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+        .header-bg {
+            background: linear-gradient(135deg, #0d6efd 0%, #003d99 100%);
+            color: white;
+            padding: 40px 0;
+            border-radius: 0 0 30px 30px;
+        }
+
+        .search-box {
+            border-radius: 50px;
+            border: none;
+            box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
+            padding: 15px 25px;
+        }
+
+        .product-card {
+            border: none;
+            border-radius: 15px;
+            transition: 0.3s;
+            background: white;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .product-card:hover {
+            transform: translateY(-5px);
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.08);
+        }
+
+        .stok-label {
+            font-size: 0.8rem;
+            color: #6c757d;
+            font-weight: 600;
+            background: #e9ecef;
+            padding: 2px 10px;
+            border-radius: 20px;
+        }
+
+        .price {
+            font-size: 1.2rem;
+            font-weight: 700;
+            color: #2d3436;
+        }
+
+        .btn-order {
+            background-color: var(--wa-color);
+            color: white;
+            border-radius: 10px;
+            font-weight: 600;
+            border: none;
+        }
+
+        .btn-order:hover {
+            background-color: #1eb956;
+            color: white;
+        }
+
+        .btn-ask {
+            background-color: var(--ask-color);
+            color: white;
+            border-radius: 10px;
+            font-weight: 600;
+            border: none;
+        }
+
+        .btn-ask:hover {
+            background-color: #ee8e2e;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="header-bg text-center mb-5">
+        <div class="container">
+            <h2 class="fw-bold"><i class="bi bi-shop me-2"></i>Katalog Produk Kami</h2>
+            <p class="opacity-75">Cari barang favorit Anda dengan mudah</p>
+            <div class="mx-auto mt-4" style="max-width: 600px;">
+                <input type="text" id="searchInput" class="form-control search-box" placeholder="Cari nama barang (Contoh: Mika, Dus, Sifon)...">
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-5">
+        <div class="row g-3" id="productGrid">
+            <?php foreach ($barang as $b): ?>
+                <div class="col-6 col-md-4 col-lg-3 product-item" data-nama="<?= strtolower($b['nama']) ?>">
+                    <div class="card product-card p-3 shadow-sm">
+                        <div class="mb-2">
+                            <span class="stok-label">Stok: <?= $b['stok'] ?></span>
+                        </div>
+                        <h6 class="fw-bold text-dark flex-grow-1"><?= htmlspecialchars($b['nama']) ?></h6>
+
+                        <div class="mt-3">
+                            <?php if ($b['harga'] > 0): ?>
+                                <div class="price mb-2">Rp <?= number_format($b['harga'], 0, ',', '.') ?></div>
+                                <a href="https://wa.me/<?= $WA_NUMBER ?>?text=<?= urlencode('Halo admin, saya mau pesan: ' . $b['nama']) ?>"
+                                    target="_blank" class="btn btn-order w-100 py-2">
+                                    <i class="bi bi-cart-plus me-1"></i> Pesan
+                                </a>
+                            <?php else: ?>
+                                <div class="text-muted small mb-2"><i class="bi bi-info-circle me-1"></i>Harga blm tersedia</div>
+                                <a href="https://wa.me/<?= $WA_NUMBER ?>?text=<?= urlencode('Halo admin, mau tanya harga: ' . $b['nama']) ?>"
+                                    target="_blank" class="btn btn-ask w-100 py-2 text-white">
+                                    <i class="bi bi-chat-dots me-1"></i> Tanyakan Harga
+                                </a>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+        <div id="noResult" class="text-center py-5 d-none">
+            <i class="bi bi-search" style="font-size: 3rem; color: #ccc;"></i>
+            <h5 class="mt-3 text-muted">Barang tidak ditemukan...</h5>
+        </div>
+    </div>
+
+    <script>
+        // FITUR SEARCH REAL-TIME
+        document.getElementById('searchInput').addEventListener('input', function() {
+            let filter = this.value.toLowerCase();
+            let items = document.querySelectorAll('.product-item');
+            let anyVisible = false;
+
+            items.forEach(item => {
+                let name = item.getAttribute('data-nama');
+                if (name.includes(filter)) {
+                    item.style.display = "";
+                    anyVisible = true;
+                } else {
+                    item.style.display = "none";
+                }
+            });
+
+            document.getElementById('noResult').classList.toggle('d-none', anyVisible);
+        });
+    </script>
+
+</body>
+
+</html>
